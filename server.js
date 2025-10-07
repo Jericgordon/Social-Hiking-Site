68ee1af047e220609c4dbc4d24331416795ca36e
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-10-07 12:34:40 -0600

    Changed syntax to better reflect module

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express');
+import postsApi from "./routes/postsApi.js";
+import express from "express"
 const app = express();
 
 app.listen(3000)
 
 app.use(express.static("public"))
+
+app.get("/api/users", (req,res) => {
+    console.log(postsApi.data);
+    res.json(postsApi);
+})
